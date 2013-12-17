8f236dce99141abe227641b2ec23490f41274e95
Dovy Paukstys <info@simplerain.com>
2013-12-17 20:02:43 +0100

    Working from the API (profile only)

---
+++ Gruntfile.js
@@ -0,0 +1,169 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    src: 'src/*.js',
+
+    clean: {
+      dist: ['dist']
+    },
+
+    jshint: {
+      src: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: ['<%= src %>']
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/unit/*.js']
+      }
+    },
+
+    karma: {
+      options: {
+        configFile: 'karma.conf.js'
+      },
+      dev: {
+        background: true,
+        browsers: ['PhantomJS']
+      },
+      travis: {
+        singleRun: true,
+        browsers: ['PhantomJS']
+      }
+    },
+    less: {
+      files: {
+        "css/style.css": "less/style.less"
+      }
+    }, 
+
+    less: {
+      development: {
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      },
+      production: {
+        options: {
+          cleancss: true
+        },
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+
+    requirejs: {
+      options: {
+        baseUrl: 'src',
+        include: ['FamilySearch'],
+        wrap: {
+          startFile: 'src/header.frag',
+          endFile: 'src/footer.frag'
+        }
+      },
+      dev: {
+        options: {
+          out: 'dist/familysearch-javascript-sdk.js',
+          optimize: 'none'
+        }
+      },
+      prod: {
+        options: {
+          out: 'dist/familysearch-javascript-sdk.min.js',
+          optimize: 'uglify2'
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          open: 'http://localhost:9000/',
+          livereload: true
+        }
+      }
+    },
+
+    watch: {
+      files: [
+        '<%= src %>',
+        'js/*.js',
+        'less/*.less',
+        'templates/**/*.html',
+        'Gruntfile.js',
+      ],
+      tasks: ['hogan', 'less:development'],
+      options: {
+        livereload: false,
+        spawn: false
+      }
+    },
+    hogan: {
+      publish: {
+        options: {
+          prettify: true,
+          namespace: 'SumoWidgets',
+          defaultName: function(file) {
+            return file.split('/').pop();
+          }
+        },
+        files:{
+          "js/sumowidgets.js": ["templates/**/*.html"]
+        }
+      }
+    }
+
+
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-hogan');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('server', [
+    'connect',
+    'karma:dev:start',
+    'watch'
+  ]);
+
+  grunt.registerTask('test', [
+    'karma:travis'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'jshint',
+    'test',
+    'ngdocs',
+    'requirejs'
+  ]);
+
+  grunt.registerTask('publish', [
+    'build',
+    'gh-pages:dev'
+  ]);
+
+  grunt.registerTask('travis', [
+    'jshint',
+    'karma:travis',
+    'ngdocs',
+    'requirejs',
+    'gh-pages:travis'
+  ]);
+
+  // Default task(s)
+  grunt.registerTask('default', ['build']);
+};
--- js/main.js
+++ js/main.js
@@ -25,86 +25,26 @@ $(document).ready(function () {
     $('.familyGroup').on('hidden.bs.collapse', toggleChevron);
     $('.familyGroup').on('shown.bs.collapse', toggleChevron);
 
-    $('.tree-family a').on('click', function (e) {
-        e.preventDefault();
-        return false;
-    });
-    var id = "KWZH-X9C";
-    if (window.location.hash) {
-        id = window.location.hash.replace('#', '');
-    }
-    personSelect(id);
-
-    $('#personSelect a').click(function () {
-    	window.location.hash = $(this).attr('href');
-        location.reload();
-        //$.when($('.spouses-children h2, .parents-children h2').hide(), $('#familyView .spouses-children .panel, #familyView .parents-children .panel').remove(), personSelect($(this).attr('href').replace('#', ''))).done(function () {
-         //   $('.spouses-children h2, .parents-children h2').fadeIn();
-        //});
-    });
-
-    function personSelect(id) {
-        $.when($.ajax('data/family/'+id + ".json"), $.ajax("templates/person.html"), $.ajax("templates/family.html"), $.ajax("templates/event.html"), $.ajax("templates/children.html")).done(function (obj, person, family, event, children) {
-            data = obj[0].data;
-            person = Hogan.compile(person[0]);
-            family = Hogan.compile(family[0]);
-            event = Hogan.compile(event[0]);
 
 
-            var theData = new Array();
-            var holder = new Object();
-            console.log(data);
-            $.each(data.spouses, function (k, spouse) {
-                spouse.husband.gender = spouse.husband.gender.toLowerCase();
-                if (!spouse.husband.name == "") {
-                	spouse.husband.name = "Unknown";
-                }
-                spouse.wife.gender = spouse.wife.gender.toLowerCase();
-                if (spouse.event) {
-                    spouse.event.type = spouse.event.type.toLowerCase();
-                    spouse.event.type = spouse.event.type.charAt(0).toUpperCase() + spouse.event.type.slice(1);
-                }
-                //var childrenString = "";
-                if (spouse.children) {
-                    var theChildren = new Array();
-                    $.each(this.children, function (x, child) {
-                        if (child.gender) {
-                            child.gender = child.gender.toLowerCase();
-                        }
-                        spouse.children[x] = child;
-                    });
-                }
-                //this.children = childrenString;
-                $('.spouses-children').append(family.render(spouse));
 
-            });
-            $.each(data.parents, function (k, spouse) {
-                spouse.husband.gender = spouse.husband.gender.toLowerCase();
-                if (!spouse.husband.name) {
-                	spouse.husband.name = "Unknown";
-                }
-                spouse.wife.gender = spouse.wife.gender.toLowerCase();
-                if (!spouse.wife.name) {
-                	spouse.wife.name = "Unknown";
-                }
-                if (spouse.event) {
-                    spouse.event.type = spouse.event.type.toLowerCase();
-                    spouse.event.type = spouse.event.type.charAt(0).toUpperCase() + spouse.event.type.slice(1);
-                }
-                //var childrenString = "";
-                if (spouse.children) {
-                    var theChildren = new Array();
-                    $.each(this.children, function (x, child) {
-                        if (child.gender) {
-                            child.gender = child.gender.toLowerCase();
-                        }
-                        spouse.children[x] = child;
-                    });
-                }
-                //this.children = childrenString;
-                $('.parents-children').append(family.render(spouse)).fadeIn();
-            });
-            return true;
-        });
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
